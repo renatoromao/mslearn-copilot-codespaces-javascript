a5bd3c4e54ed6245e9cc1ad836ef8b2f19fbdc18
Renato Romão <renato.romao@hotmail.com>
2025-08-13 10:30:26 +0000

    Enhance Home component and styles with Accenture branding and layout adjustments

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -26,14 +26,12 @@ const imageAltText = "Adult female in office setting leaning against a glass wal
 
 const Home = ({ name, title }) => {
   return (
-    <section id="home" className="min-height">
+    <section id="home" className="acc_home section min-height padding">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{name}</h1>
-        <h2>{title}</h2>
-      </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      <div className="acc_home_content">
+        <h1 className="acc_home_title">{name}</h1>
+        <h2 className="acc_home_subtitle">{title}</h2>
+        <img src={arrowSvg} className="acc_arrow" alt={imageAltText} />
       </div>
     </section>
   );
